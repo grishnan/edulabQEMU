19105fbf47559eb7233cf998e207d97b06ffdbdd
grishnan <grishnan@gmail.com>
2020-01-03 23:42:38 +0300

    Adding PreInstall section in README, where I explain how to place virtual machines right. Revision and refactoring run.sh and startlab.

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 Virtual laboratory for educational purposes aimed at starting of typical virtualized network environments by means of QEMU
 
 # Prerequisities:
-
 QEMU version 4.1.0
 
+# PreInstall:
+1. cd to edulabQEMU
+2. mkdir machines; cd machines; mkdir 1 2 3 4 5 6 base; cd base
+3. Create linux.qcow2, windows.qcow2, windows_server.qcow2 virtual machines. These are base VMs.
+4. Now you can place snapshots of the base VMs in dirs machines/1, machines/2, etc. Snapshots names in the machines/n must according to n configuration scheme (look at function print_scheme from run.sh)
 
 # Run laboratory:
-
 1. cd to edulabQEMU
 2. chmod +x qemu-ifup qemu-ifdown startlab run.sh
 3. sudo ./run.sh
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # E-mail: grishnan@gmail.com
 # License: GNU GENERAL PUBLIC LICENSE Version 3
 
-CA=2 # configurations amount
+CA=6 # configurations amount
 
 function check_root_privileges {
   if [ $EUID -ne 0 ]; then
@@ -16,7 +16,7 @@ function check_root_privileges {
 
 function print_scheme () {
   case $1 in
-    1) 
+    1|3|5) 
       echo
       echo -e "\033[1mCLI-C\033[0m"
       echo -e "     \\"
@@ -24,7 +24,7 @@ function print_scheme () {
       echo -e "     /"
       echo -e "\033[1mCLI-P\033[0m"
       echo ;;
-    2)
+    2|4|6)
       echo
       echo -e "\033[1mCLI-C\033[0m — br0 — \033[1mSRV\033[0m — WAN"
       echo ;;
@@ -44,7 +44,7 @@ function print_help {
 function startlab {
   read -p "Run this configuration? (y/n): " answer
   case $answer in
-    y|Y) ./startlab; exit 0 ;;
+    y|Y) ./startlab $1; exit 0 ;;
     *) echo ;;
   esac
 }
@@ -58,12 +58,15 @@ do
   echo
   echo "1. Private virtual bridge. Only debian. Access to WAN via server. Two clients."
   echo "2. Private virtual bridge. Only debian. Access to WAN via server. One client."
-  echo "3. Third configuration."
+  echo "3. Private virtual bridge. Only windows. Access to WAN via server (GUI). Two clients."
+  echo "4. Private virtual bridge. Only windows. Access to WAN via server (GUI). One client."
+  echo "5. Private virtual bridge. Only windows. Access to WAN via server (Core). Two clients."
+  echo "6. Private virtual bridge. Only windows. Access to WAN via server (Core). One client."
   echo
   read -p "Your option (h for help): " option
 
   if [[ $(seq $CA) =~ (^|[[:space:]])$option($|[[:space:]]) ]]; then
-    if check_root_privileges; then print_scheme $option; startlab; fi
+    if check_root_privileges; then print_scheme $option; startlab $option; fi
   else
     case $option in
       h) print_help ;;
